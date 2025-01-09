bf40a2d38dde8265ee48e0b0e72d6f441f12ac31
Sunzzx6969 <sultanmuhammad5758@gmail.com>
2025-01-09 08:50:14 +0700

    BACKEND_LARAVEL

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+return new class extends Migrations
 {
     /**
      * Run the migrations.
@@ -12,9 +12,12 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->integer("id")->autoIncrement();
             $table->string('name');
             $table->string('email')->unique();
+            $table->enum('role',["Admin","Kasir","Distributor","Cleaning service"]);
+            $table->string("address");
+            $table->date('birthday');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
